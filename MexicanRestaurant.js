794819e17b17ad471511887ebfbedc226113f46a
Dana Freitas <danarobertfreitas@gmail.com>
2024-03-06 16:52:08 -0500

    enchance the code for the arrays

--- MexicanRestaurant.js
+++ MexicanRestaurant.js
@@ -1,45 +1,43 @@
 const ham = document.getElementById("hamburger");
- const holder = document.getElementById("burgerholder");
+const holder = document.getElementById("burgerholder");
 
 const title = document.getElementById("title");
 
 title.addEventListener("click", (event) => {
-window.location.href="index.html";
-})
+  window.location.href = "index.html";
+});
 
-const pagename = document.getElementsByClassName("header__defaultmenu--item");
+const defaultMenuButton =      document.getElementsByClassName("header__defaultmenu--item");
+const responsiveMenuButton =   document.getElementsByClassName("header__responsivemenu--item");
+const defaultMenuArray = Array.from(defaultMenuButton);
+const responsiveMenuArray = Array.from(responsiveMenuButton);
 
-console.log(pagename)
-
-//location.href="index.html"  
-let test = []
-
-console.log(test)
-for (let i = 0; i < pagename.length; i++){
-  console.log(pagename[i])
-let thehref=pagename[i].getAttribute("href");
-test.push(thehref)
-}
-console.log(test)
+const fusedMenuArray = defaultMenuArray.concat(responsiveMenuArray);
+console.log(fusedMenuArray)
 
 
 
-
-//have var ref the href
-
-
-for (let i = 0; i < pagename.length; i++){
+const grave = document.getElementsByClassName("grave__Grave--CrossWrapper");
 
 
-pagename[i].addEventListener("click",() => {
-let thehref=pagename[i].getAttribute("href");
-let hrefslice = thehref.slice(1);
-window.location.href=`${hrefslice}.html`;
+let defaultlocationarray = [];
+let 
 
-
- })
+for (let i = 0; i < defaultMenuButton.length; i++) {
+  console.log(defaultMenuButton[i]);
+  let thehref = defaultMenuButton[i].getAttribute("href");
+  test.push(thehref);
+  console.log(test)
 }
 
+for (let i = 0; i < defaultMenuButton.length; i++) {
+  defaultMenuButton[i].addEventListener("click", () => {
+    let thehref = defaultMenuButton[i].getAttribute("href");
+    let hrefslice = thehref.slice(1);
+    window.location.href = `${hrefslice}.html`;
+    test = []
+  });
+}
 
 
 
@@ -52,68 +50,57 @@ window.location.href=`${hrefslice}.html`;
 //     behavior: "smooth",
 //     block: "start",
 //     inline: "start",
-//   }); 
+//   });
 // });
 
-const grave = document.getElementsByClassName(
-  "grave__Grave--CrossWrapper"
-);
 
-const defaultMenuButton = document.getElementsByClassName("header__defaultmenu--item")
-const responsiveMenuButton = document.getElementsByClassName("header__responsivemenu--item")
 
-    //  let section = document.getElementsByClassName("section");
-    //  let totalSectionHeight = 0;
-    //  for (let i = 0; i < sections.length; i++) {
-    //    totalSectionHeight += sections[i].offsetHeight;
-    //  }
-     
-     // Set your variable 1000 pixels below the total section height
-     //let adjustedsection = totalSectionHeight + 1000;
+//  let section = document.getElementsByClassName("section");
+//  let totalSectionHeight = 0;
+//  for (let i = 0; i < sections.length; i++) {
+//    totalSectionHeight += sections[i].offsetHeight;
+//  }
 
-     let section = document.getElementsByClassName("section");
-     function getElementPositionRelativeToDocument(section) {
+// Set your variable 1000 pixels below the total section height
+//let adjustedsection = totalSectionHeight + 1000;
 
- 
-     let rect = section.getBoundingClientRect();
-     let scrollTop = window.scrollY || document.documentElement.scrollTop;
- 
-     let topRelativeToDocument = rect.top + scrollTop;
- 
-     return topRelativeToDocument;
- }
+let section = document.getElementsByClassName("section");
+function getElementPositionRelativeToDocument(section) {
+  let rect = section.getBoundingClientRect();
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
 
- const heropadding = document.getElementById('header').clientHeight;
+  let topRelativeToDocument = rect.top + scrollTop;
 
+  return topRelativeToDocument;
+}
+
+const heropadding = document.getElementById("header").clientHeight;
 
 for (let k = 0; k < responsiveMenuButton.length; k++) {
   responsiveMenuButton[k].addEventListener("click", (event) => {
-     event.preventDefault();
+    event.preventDefault();
 
+    let topRelativeToDocument = getElementPositionRelativeToDocument(
+      section[k]
+    );
 
-     let topRelativeToDocument = getElementPositionRelativeToDocument(section[k])
-
-     let offsetPosition = topRelativeToDocument - heropadding //- headerOffset;
-console.log(`offsetposition ${offsetPosition}  is ${heropadding} minus ${topRelativeToDocument}`)
-     window.scrollTo({top: offsetPosition, behavior: 'smooth'});
-  }); 
+    let offsetPosition = topRelativeToDocument - heropadding; //- headerOffset;
+    console.log(
+      `offsetposition ${offsetPosition}  is ${heropadding} minus ${topRelativeToDocument}`
+    );
+    window.scrollTo({ top: offsetPosition, behavior: "smooth" });
+  });
 }
-
-
-
-
 
 ham.addEventListener("click", () => {
   ham.classList.toggle("show");
 
-if (!slidemenu.classList.contains("menuclassenter") &&     !slidemenu.classList.contains("menuclassexit"))
-  
-  { 
-  slidemenu.classList.add("menuclassenter")
-}
-
-
-
+  if (
+    !slidemenu.classList.contains("menuclassenter") &&
+    !slidemenu.classList.contains("menuclassexit")
+  ) {
+    slidemenu.classList.add("menuclassenter");
+  }
 
   if (
     slidemenu.classList.contains("menu_slide") ||
@@ -121,17 +108,11 @@ if (!slidemenu.classList.contains("menuclassenter") &&     !slidemenu.classList.
   ) {
     slidemenu.classList.remove("menu_slide");
 
-   
-     slidemenu.classList.remove("menuclassexit");
-     slidemenu.classList.add("menuclassenter");
-
-
-
+    slidemenu.classList.remove("menuclassexit");
+    slidemenu.classList.add("menuclassenter");
   } else if (slidemenu.classList.contains("menuclassenter")) {
-
     slidemenu.classList.add("menuclassexit");
     slidemenu.classList.remove("menuclassenter");
-
 
     document.getElementById("hamburger").style.width = "";
   }
@@ -140,26 +121,20 @@ if (!slidemenu.classList.contains("menuclassenter") &&     !slidemenu.classList.
 for (let l = 0; l < defaultMenuButton.length; l++) {
   defaultMenuButton[l].addEventListener("click", (event) => {
     event.preventDefault();
-     let section = document.getElementsByClassName("section");
+    let section = document.getElementsByClassName("section");
 
-
-
-//    section[l].scrollIntoView({
-   //   behavior: "smooth",
-  //    block: "start",
+    //    section[l].scrollIntoView({
+    //   behavior: "smooth",
+    //    block: "start",
     //  inline: "nearest",
     //});
 
-
-    const respon = document.getElementsByClassName(
-      "header__defaultmenu--item"
-    );
-      document.getElementById("hamburger").classList.toggle("show");
-      slidemenu.classList.add("menuclassexit");
-      slidemenu.classList.remove("menuclassenter");
-      document.getElementById("hamburger").style.width = "";
-    }
-  );
+    const respon = document.getElementsByClassName("header__defaultmenu--item");
+    document.getElementById("hamburger").classList.toggle("show");
+    slidemenu.classList.add("menuclassexit");
+    slidemenu.classList.remove("menuclassenter");
+    document.getElementById("hamburger").style.width = "";
+  });
 }
 
 const keyframesRule = `
@@ -173,12 +148,6 @@ const keyframesRule = `
       }
     `;
 
-
-
-
-
-
-
-    const ImageGallery = []
+const ImageGallery = [];
 
 const ImageBackground = document.getElementsByClassName("imagecontainer");
